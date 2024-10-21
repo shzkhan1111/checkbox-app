62f1c1338d8963711374ed5aa872302312ff2f98
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2024-10-21 20:34:50 +0500

    chnages in sample data to make it distinct

--- src/app/posts/state/post.state.ts
+++ src/app/posts/state/post.state.ts
@@ -8,7 +8,7 @@ export interface PostState{
 export const initialState : PostState = {
   posts:[
     {id:'1' , title: 'Sample Title 1' , description: 'Sample Description 1'},
-    {id:'1' , title: 'Sample Title 1' , description: 'Sample Description 1'}
+    {id:'1' , title: 'Sample Title 2' , description: 'Sample Description 2'}
 
   ]
 }
